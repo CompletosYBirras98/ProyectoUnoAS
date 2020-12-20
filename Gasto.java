9acc8c7b794f6a0d8385621750e29ff94ade0753
Alex Sarabia 1 <hijosdelosbits@gmail.com>
2020-12-20 20:35:25 +0100

    Implementando gastos

--- Gasto.java
+++ Gasto.java
@@ -2,5 +2,19 @@ package com.mycompany.proyectounoas;
 
 
 public class Gasto {
+    private String tipo;
+    private int valor;
+    private boolean pagado;
+
+    public Gasto(String tipo, int valor, boolean pagado) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.pagado=pagado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+    
     
 }
--- main.java
+++ main.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.proyectounoas;
 
-/**
- *
- * @author Alex
- */
+import java.util.ArrayList;
+
+
 public class main {
-    
+    public static void main(String[] args) {
+        ArrayList<Gasto> lista= new ArrayList<Gasto>();
+ 
+        lista.add(new Gasto("Luz",12000));
+        lista.add(new Gasto("Agua",7000));
+        lista.add(new Gasto("Internet",30000));
+        lista.add(new Gasto("Gas",20000));
+ 
+        /*No compila esto:
+        double resultado=lista.stream().mapToDouble(gasto-gasto.getValor()*1.21)
+                        .filter(gasto->gasto<20000)
+                        .sum();
+        */
+        
+        //Lo haré denuevo
+        
+        
+        double resultado=0;
+        for (int i = 0; i < lista.size(); i++) {
+            if(lista.get(i).getValor()<20000){
+                resultado=resultado+lista.get(i).getValor() + (lista.get(i).getValor()*0.21);
+            }else{
+                resultado=resultado+lista.get(i).getValor();
+            }
+            
+        }
+        
+        System.out.println(resultado);
+        
+        
+
+    }
 }
